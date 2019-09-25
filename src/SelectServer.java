0d4dec9bbbb34f2cb3777cee0f9ce4036700f9c3
Mariella Nalepa <mariellanalepa@gmail.com>
2019-09-25 12:41:31 -0600

    Moved lengthy sequences to helper functions

--- src/SelectServer.java
+++ src/SelectServer.java
@@ -17,10 +17,42 @@ import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.nio.charset.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class SelectServer {
-    public static int BUFFERSIZE = 32;
+	
+	private static int BUFFERSIZE = 32;
+	
+    // Move these object declarations here so they can be accessed
+	// by both main and static helper methods
+	// Static since they belong to the class rather than an instance
+	// of the class since we are essentially following a procedural
+	// design
+	// => => => 
+	// Initialize buffers and coders for receive and send on channels
+    private static String command = "";	
+    private static Charset charset = Charset.forName( "us-ascii" );  
+    private static CharsetDecoder decoder = charset.newDecoder();  
+    private static CharsetEncoder encoder = charset.newEncoder();
+    private static ByteBuffer inByteBuffer = null;
+    private static CharBuffer inCharBuffer = null;
+    private static ByteBuffer outByteBuffer = null;
+    private static CharBuffer outCharBuffer = null;
+    private static int bytesSent, bytesRecv;     // number of bytes sent or received
+    private static DatagramPacket packet = null; //datagram packet for DatagramChannel
+    
+    // Initialize the selector and channels
+    private static Selector selector = null;
+    private static ServerSocketChannel tcpChannel;
+    private static DatagramChannel udpChannel;
+    private static SelectableChannel rdyChannel; // for when we iterate through the selector ready key set
+    private static SelectionKey key;
+    
+    
     public static void main(String args[]) throws Exception 
     {
         if (args.length != 1)
@@ -29,27 +61,30 @@ public class SelectServer {
             System.exit(1);
         }
 
-        // Initialize buffers and coders for receive and send on channels
-        String line = "";	
+        /*
+         * // Initialize buffers and coders for receive and send on channels
+        String command = "";	
         Charset charset = Charset.forName( "us-ascii" );  
         CharsetDecoder decoder = charset.newDecoder();  
         CharsetEncoder encoder = charset.newEncoder();
-        ByteBuffer inBuffer = null;
-        CharBuffer cBuffer = null;
+        ByteBuffer inByteBuffer = null;
+        CharBuffer inCharBuffer = null;
+        ByteBuffer outByteBuffer = null;
+        CharBuffer outCharBuffer = null;
         int bytesSent, bytesRecv;     // number of bytes sent or received
         // Declare a datagram packet for DatagramChannel
         DatagramPacket packet = null;
-        
+        */
         
         // Initialize the selector
         Selector selector = Selector.open();
-
+        
         /****************************************************************
          * FOR TCP: Create channel to listen for incoming TCP connections
          ****************************************************************/
         
-        // Create a server socket channel and make it non-blocking
-        ServerSocketChannel tcpChannel = ServerSocketChannel.open();	// open but not yet bound
+        // Open server socket channel and make it non-blocking
+        tcpChannel = ServerSocketChannel.open();	// open but not yet bound
         tcpChannel.configureBlocking(false);
         
         // Create socket address with specified port number and wildcard address 
@@ -61,8 +96,8 @@ public class SelectServer {
          * FOR UDP: Create channel that can send and receive UDP packets
          ****************************************************************/
 
-        // Create a datagram (socket) channel and make it non-blocking
-        DatagramChannel udpChannel = DatagramChannel.open();
+        // Open datagram socket channel and make it non-blocking
+        udpChannel = DatagramChannel.open();
         udpChannel.configureBlocking(false);
         
         // Bind the datagram socket to the same address (port) as the server socket
@@ -93,13 +128,13 @@ public class SelectServer {
                 while (readyItor.hasNext()) 
                 {
                     // Get key from set
-                    SelectionKey key = (SelectionKey)readyItor.next();
+                    key = (SelectionKey)readyItor.next();
 
                     // Remove current entry
                     readyItor.remove();
                     
                     // Get the channel of the ready key
-                    SelectableChannel channel = key.channel();
+                    rdyChannel = key.channel();
                     //System.out.println("What kind of channel am I?");
                     //System.out.println(channel.getClass().getName());
 
@@ -107,6 +142,8 @@ public class SelectServer {
                     // If this key's channel is ready to accept a new socket connection
                     if (key.isAcceptable()) 	
                     {
+                    		acceptClientConnection();
+                    		/*
                         // Accept a connection made to this channel's socket
                         SocketChannel clientChannel = ((ServerSocketChannel)channel).accept();
                         clientChannel.configureBlocking(false);
@@ -114,21 +151,26 @@ public class SelectServer {
                         
                         // Register the new connection for read operation
                         clientChannel.register(selector, SelectionKey.OP_READ);
+                        */
                     } 
                     // If the key's channel is ready for reading
                     else if (key.isReadable()) 
                     {
                     		// Open input and output streams
-                        inBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
-                        cBuffer = CharBuffer.allocate(BUFFERSIZE);
+                        inByteBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
+                        inCharBuffer = CharBuffer.allocate(BUFFERSIZE);
+                        outByteBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
+                        outCharBuffer = CharBuffer.allocate(BUFFERSIZE);	
                         
                     		// If there is a datagram waiting at the UDP socket
-                    		if (channel instanceof DatagramChannel)
+                    		if (rdyChannel instanceof DatagramChannel)
 						{
-                    			DatagramChannel udpChan = (DatagramChannel) channel;
+                    			receiveDatagram();
+                    			/*
+                    			DatagramChannel udpChan = (DatagramChannel) rdyChannel;
                     			// Receive datagram available on this channel 
                     			// Returns the datagram's source address
-                    			SocketAddress clientAdd = udpChan.receive(inBuffer);
+                    			SocketAddress clientAdd = udpChan.receive(inByteBuffer);
                     			
                     			// How to do error checking on receive?
                     			
@@ -136,25 +178,29 @@ public class SelectServer {
                     			{
                     				// make buffer ready for a new sequence of channel-write
                     				// or relative get operations
-                    				inBuffer.flip();	// make buffer available    	
-                    				decoder.decode(inBuffer, cBuffer, false);
-                    				cBuffer.flip();		
-                    				line = cBuffer.toString();
-                    				System.out.println("Client: " + line);
+                    				inByteBuffer.flip();	// make buffer available    	
+                    				decoder.decode(inByteBuffer, inCharBuffer, false);
+                    				inCharBuffer.flip();		
+                    				command = inCharBuffer.toString();
+                    				System.out.println("UDP Client: " + command);
                     				
                     				// echo the message back
-                    				inBuffer.flip();
-                    				udpChan.send(inBuffer,clientAdd);
+                    				inByteBuffer.flip();
+                    				udpChan.send(inByteBuffer,clientAdd);
                     			}
+                    			*/
 						}
                     		
                     		// Else if a TCP client socket channel is ready for reading
 						else 
 						{
-							SocketChannel clientChannel = (SocketChannel) channel;
+							if (!readFromClientChannel())
+								continue;
+							/*
+							SocketChannel clientChannel = (SocketChannel) rdyChannel;
                   
                             // Read from client socket channel
-                            bytesRecv = clientChannel.read(inBuffer);
+                            bytesRecv = clientChannel.read(inByteBuffer);
                             if (bytesRecv <= 0)
                             {
                                 System.out.println("read() error, or connection closed");
@@ -164,26 +210,73 @@ public class SelectServer {
                             
                             // make buffer ready for a new sequence of channel-write
                             // or relative get operations
-                            inBuffer.flip();	// make buffer available    	
-                            decoder.decode(inBuffer, cBuffer, false);
-                            cBuffer.flip();		
-                            line = cBuffer.toString();
-                            System.out.println("Client: " + line);
+                            inByteBuffer.flip();	// make buffer available    	
+                            decoder.decode(inByteBuffer, inCharBuffer, false);
+                            inCharBuffer.flip();		
+                            command = inCharBuffer.toString();
+                            System.out.println("TCP Client: " + command);
                             
                             // Echo the message back
-                            inBuffer.flip();
+                            inByteBuffer.flip();
                             
-                            bytesSent = clientChannel.write(inBuffer); 
+                            bytesSent = clientChannel.write(inByteBuffer); 
                             if (bytesSent != bytesRecv)
                             {
                                 System.out.println("write() error, or connection closed");
                                 key.cancel();  // deregister the socket
                                 continue;
                             }
+                            */
 						}
                         
-                        if (line.equals("terminate\n"))
+                        if (command.equals("terminate\n"))
                             terminated = true;
+                        
+                        /*
+                        else if (command.equals("list\n"))
+                        {
+                                String dirName = System.getProperty("user.dir");
+                                File[] files = new File(dirName).listFiles();
+                                
+                                for (File file : files)
+                                {
+                                		// Put filename in Char Buffer
+                                		outCharBuffer.put(file.getName());
+                                		// Send filename to Client
+                                		//if ()
+                                		//sendToUDPClient(outCharBuffer);
+                                }
+                        }
+                        else 
+                        {
+                        		String[] words = command.split("\\s+");
+                        		if (words[0].equals("get") && words.length == 2)
+                        		{
+                        			try
+                        			{
+                        				String fileName = words[1];
+                        				BufferedReader reader = new BufferedReader(new FileReader(fileName));
+                        				String fileLine;
+                        				
+                        				while ((fileLine = reader.readLine()) != null)
+                        				{
+                        					// Send file contents to client a line at a time
+                        				}
+                        				
+                        				reader.close();
+                        			}
+                        			catch (Exception e)
+                        			{
+                        				String errMsg = "Error in opening file <filename>";
+                        				// Send error message to client
+                        			}
+                        			
+                        		}
+                        }
+                        */
+                        
+                    
+
                         
                     } // end of else if (key.isReadible())
                 } // end of while (readyItor.hasNext()) 
@@ -206,4 +299,92 @@ public class SelectServer {
                 ((SocketChannel)key.channel()).socket().close();
         }
     }
+    
+    // HELPER METHODS
+    public static void acceptClientConnection() throws Exception
+    {
+    		// Accept a connection made to this channel's socket
+        SocketChannel clientChannel = ((ServerSocketChannel)rdyChannel).accept();
+        clientChannel.configureBlocking(false);
+        System.out.println("Accept conncection from " + clientChannel.socket().toString());
+        
+        // Register the new connection for read operation
+        clientChannel.register(selector, SelectionKey.OP_READ);
+    }
+    
+    public static void receiveDatagram() throws Exception
+    {
+    		DatagramChannel udpChan = (DatagramChannel) rdyChannel;
+		// Receive datagram available on this channel 
+		// Returns the datagram's source address
+		SocketAddress clientAdd = udpChan.receive(inByteBuffer);
+		
+		// How to do error checking on receive?
+		
+		if (clientAdd != null)
+		{
+			// make buffer ready for a new sequence of channel-write
+			// or relative get operations
+			inByteBuffer.flip();	// make buffer available    	
+			decoder.decode(inByteBuffer, inCharBuffer, false);
+			inCharBuffer.flip();		
+			command = inCharBuffer.toString();
+			System.out.println("UDP Client: " + command);
+			
+			// echo the message back
+			inByteBuffer.flip();
+			udpChan.send(inByteBuffer,clientAdd);
+		}
+    }
+    
+    public static boolean readFromClientChannel() throws Exception
+    {
+    		SocketChannel clientChannel = (SocketChannel) rdyChannel;
+        
+        // Read from client socket channel
+        bytesRecv = clientChannel.read(inByteBuffer);
+        if (bytesRecv <= 0)
+        {
+            System.out.println("read() error, or connection closed");
+            key.cancel();  // deregister the socket
+            //continue;
+            return false;	// operation was not successful
+        }
+        
+        // make buffer ready for a new sequence of channel-write
+        // or relative get operations
+        inByteBuffer.flip();	// make buffer available    	
+        decoder.decode(inByteBuffer, inCharBuffer, false);
+        inCharBuffer.flip();		
+        command = inCharBuffer.toString();
+        System.out.println("TCP Client: " + command);
+        
+        // Echo the message back
+        inByteBuffer.flip();
+        
+        bytesSent = clientChannel.write(inByteBuffer); 
+        if (bytesSent != bytesRecv)
+        {
+            System.out.println("write() error, or connection closed");
+            key.cancel();  // deregister the socket
+            //continue;
+            return false;
+        }
+        
+        return true;
+    }
+   
+    public static void sendToUDPClient(CharBuffer outCharBuffer)
+    {
+    		outCharBuffer.flip();	// flip buffer: limit is set to current position and position to zero
+    		
+    }
+    
+    public static void sendToTCPClient(CharBuffer outCharBuffer)
+    {
+    		outCharBuffer.flip();	// flip buffer: limit is set to current position and position to zero
+    		
+    }
+    
+    
 }
